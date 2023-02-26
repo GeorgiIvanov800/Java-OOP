9133a57b648e6eeaa2e28b1044b6e5fefea3998c
Georgi Ivanov <gogata92@pm.me>
2023-02-26 15:01:05 +0100

    New Package in Working with abstraction
    new tasks CardRank working with enumerations

---
+++ Tasks/src/WorkingWithAbstraction/CardRanks/CardRankMain.java
@@ -0,0 +1,22 @@
+package WorkingWithAbstraction.CardRanks;
+
+public class CardRankMain {
+    enum CardRanks {
+        ACE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+    }
+    public static void main(String[] args) {
+
+    }
+}
--- Tasks/src/WorkingWithAbstraction/CardSuits/CardSuitsMain.java
+++ Tasks/src/WorkingWithAbstraction/CardSuits/CardSuitsMain.java
@@ -11,6 +11,7 @@ public class CardSuitsMain {
     public static void main(String[] args) {
         CardSuits[] values = CardSuits.values();
         System.out.println("Card Suits:");
+
         for (CardSuits value : values) {
             System.out.println("Ordinal value: " + value.ordinal() + "; Name value: " + value);
         }
