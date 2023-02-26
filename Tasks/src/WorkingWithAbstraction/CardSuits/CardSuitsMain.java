1b164a2810c73248bbff37452c8243216c0378e3
Georgi Ivanov <gogata92@pm.me>
2023-02-26 14:42:06 +0100

    Add Package Working with Abstractions

---
+++ Tasks/src/WorkingWithAbstraction/CardSuits/CardSuitsMain.java
@@ -0,0 +1,14 @@
+package WorkingWithAbstraction.CardSuits;
+
+public class CardSuitsMain {
+    enum CardSuits {
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
