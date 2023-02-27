28e15b8b670de2773d659ea11b006e179d2a512a
Georgi Ivanov <gogata92@pm.me>
2023-02-27 16:00:15 +0100

    Create new class Salary

--- Tasks/src/Encapsulation/SortByNameAndAge/Person.java
+++ Tasks/src/Encapsulation/SortByNameAndAge/Person.java
@@ -4,12 +4,17 @@ public class Person {
     private String firstName;
     private String lastName;
     private int age;
+    private  double salary;
 
 
-    public Person(String firstName, String lastName, int age) {
+     Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+    }
+    public Person(String firstName, String lastName, int age, double salary) {
+         this(firstName,lastName,age);
+         this.salary = salary;
     }
 
     String getFirstName() {
