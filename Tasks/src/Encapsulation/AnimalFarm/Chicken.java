580e7069e677765913f92e73c560e93eaf110054
Georgi Ivanov <gogata92@pm.me>
2023-02-28 13:33:09 +0100

    Update Chicken.java

--- Tasks/src/Encapsulation/AnimalFarm/Chicken.java
+++ Tasks/src/Encapsulation/AnimalFarm/Chicken.java
@@ -1,4 +1,26 @@
 package Encapsulation.AnimalFarm;
 
 public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken() {
+
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        if (age < 0 || age >= 5) {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+
+        this.age = age;
+    }
 }
