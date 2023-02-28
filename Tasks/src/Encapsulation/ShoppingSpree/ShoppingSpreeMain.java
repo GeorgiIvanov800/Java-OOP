d7a97e75cd694f706ea0cb874ce40e8eae1b3b88
Georgi Ivanov <gogata92@pm.me>
2023-02-28 14:44:49 +0100

    Update methods
    Update method cost in Class Product

--- Tasks/src/Encapsulation/ShoppingSpree/ShoppingSpreeMain.java
+++ Tasks/src/Encapsulation/ShoppingSpree/ShoppingSpreeMain.java
@@ -2,6 +2,10 @@ package Encapsulation.ShoppingSpree;
 
 public class ShoppingSpreeMain {
     public static void main(String[] args) {
+        Product product = new Product("   ",1);
+        Product bread = new Product("milk", 2);
 
+        System.out.println(bread.getName());
+        System.out.println(product.getCost());
     }
 }
