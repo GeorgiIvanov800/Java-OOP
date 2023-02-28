c37a58cb9c73ae161e6c70c8de3f95e9b118f895
Georgi Ivanov <gogata92@pm.me>
2023-02-28 13:44:54 +0100

    Update Animal Farm

--- Tasks/src/Encapsulation/AnimalFarm/AnimalFarmMain.java
+++ Tasks/src/Encapsulation/AnimalFarm/AnimalFarmMain.java
@@ -1,4 +1,15 @@
 package Encapsulation.AnimalFarm;
 
+import java.util.Scanner;
+
 public class AnimalFarmMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        Chicken chicken = new Chicken(name, age);
+        System.out.println(chicken.toString());
+    }
 }
--- Tasks/src/Encapsulation/AnimalFarm/Chicken.java
+++ Tasks/src/Encapsulation/AnimalFarm/Chicken.java
@@ -4,8 +4,9 @@ public class Chicken {
     private String name;
     private int age;
 
-    public Chicken() {
-
+    public Chicken(String name, int age) {
+        this.setName(name);
+        this.setAge(age);
     }
 
     private void setName(String name) {
@@ -17,10 +18,30 @@ public class Chicken {
     }
 
     private void setAge(int age) {
-        if (age < 0 || age >= 5) {
+        if (age < 0 || age >= 15) {
             throw new IllegalArgumentException("Age should be between 0 and 15.");
         }
 
         this.age = age;
+    }
+
+    public double productPerDay() {
+        return calculateProductPerDay();
+    }
+
+    private double calculateProductPerDay() {
+        if (this.age <= 5) {
+            return 2;
+        } else if (this.age <= 11) {
+            return 1;
+        } else {
+            return 0.75;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.", this.name, this.age, productPerDay());
+        //return "Chicken " + this.name + " (age " + this.age +  ") can produce " + productPerDay() + " eggs per day.";
     }
 }
