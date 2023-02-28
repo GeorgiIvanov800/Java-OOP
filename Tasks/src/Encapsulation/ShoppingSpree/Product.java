b866432bc0ea96bfe678d00b7b193e79895b6dd1
Georgi Ivanov <gogata92@pm.me>
2023-02-28 14:34:39 +0100

    Update Product.java
    Created the needed methods in Product Class

--- Tasks/src/Encapsulation/ShoppingSpree/Product.java
+++ Tasks/src/Encapsulation/ShoppingSpree/Product.java
@@ -1,4 +1,27 @@
 package Encapsulation.ShoppingSpree;
 
 public class Product {
+    private String name;
+    private double cost;
+
+    public Product(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    private void setCost(double cost) {
+        throw  new IllegalArgumentException("TODO");
+    }
+
+    private void setName(String name) {
+        throw  new IllegalArgumentException("TODO");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getCost() {
+        return this.cost;
+    }
 }
