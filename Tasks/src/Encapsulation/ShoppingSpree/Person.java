533d1ad5536017aaa32f0e7ab608f571333943fc
Georgi Ivanov <gogata92@pm.me>
2023-02-28 14:32:30 +0100

    Update Person.java
    created methods in Person class

--- Tasks/src/Encapsulation/ShoppingSpree/Person.java
+++ Tasks/src/Encapsulation/ShoppingSpree/Person.java
@@ -1,4 +1,30 @@
 package Encapsulation.ShoppingSpree;
 
+import java.util.List;
+
 public class Person {
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person(String name, double money) {
+        throw  new IllegalArgumentException("TODO");
+    }
+
+    private void setName() {
+        throw  new IllegalArgumentException("TODO");
+    }
+    private void setMoney() {
+        throw  new IllegalArgumentException("TODO");
+    }
+
+    public void byeProduct() {
+        throw  new IllegalArgumentException("TODO");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+
 }
