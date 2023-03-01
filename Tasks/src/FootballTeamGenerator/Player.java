5ecb7e0c7bfc0c4bf0639fc17f08173af4c30967
Georgi Ivanov <gogata92@pm.me>
2023-03-01 13:37:11 +0100

    Update Player.java
    Add methods in Player class

--- Tasks/src/FootballTeamGenerator/Player.java
+++ Tasks/src/FootballTeamGenerator/Player.java
@@ -1,4 +1,47 @@
 package FootballTeamGenerator;
 
 public class Player {
+
+    private String name;
+    private int endurance;
+    private int spring;
+    private int dribble;
+    private int passing;
+    private int shooting;
+
+    public Player(String name, int endurance, int spring, int dribble, int passing, int shooting) {
+        throw new IllegalStateException("TODO");
+    }
+
+    private void setName(String name) {
+        throw new IllegalStateException("TODO");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setEndurance(int endurance) {
+        throw new IllegalStateException("TODO");
+    }
+
+    private void setSpring(int spring) {
+        throw new IllegalStateException("TODO");
+    }
+
+    private void setDribble(int dribble) {
+        throw new IllegalStateException("TODO");
+    }
+
+    private void setPassing(int passing) {
+        this.passing = passing;
+    }
+
+    private void setShooting(int shooting) {
+        this.shooting = shooting;
+    }
+
+    private double overallSKilLevel() {
+        throw new IllegalStateException("TODO");
+    }
 }
