97bd6dfd93179854ea090065d2e9afaa67ed3bec
Georgi Ivanov <gogata92@pm.me>
2023-03-01 13:31:02 +0100

    Created new Task in Encapsulation Package
    Created task Football Team Generator

---
+++ Tasks/src/FootballTeamGenerator/Team.java
@@ -0,0 +1,14 @@
+package FootballTeamGenerator;
+
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Player> players;
+
+    public Team(String name) {
+        throw new IllegalStateException("TODO");
+    }
+
+    
+}
