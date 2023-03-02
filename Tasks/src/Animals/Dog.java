8baac39caf26e04f1334d49b9b4a9b3896b72b43
Georgi Ivanov <gogata92@pm.me>
2023-03-02 13:31:53 +0100

    Package Animals
    Created 2 Classes Animal and Dog
    Exercising Inheritance in Java

--- Tasks/src/Animals/Dog.java
+++ Tasks/src/Animals/Dog.java
@@ -1,5 +1,7 @@
 package Animals;
 
 public class Dog extends Animal {
-    
+    public void bark() {
+        System.out.println("woof");
+    }
 }
--- Tasks/src/Animals/Main.java
+++ Tasks/src/Animals/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal();
         animal.eat();
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
     }
 }
