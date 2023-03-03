350f3e7d006902450c5b9a6aefc659b358dc6143
Georgi Ivanov <gogata92@pm.me>
2023-03-03 08:32:46 +0100

    Created new Task NeedForSpeed

---
+++ Tasks/src/NeedForSpeed/RaceMotorcycle.java
@@ -0,0 +1,9 @@
+package NeedForSpeed;
+
+public class RaceMotorcycle extends Motorcycle {
+    public static double DEFAULT_FUEL_CONSUMPTION = 8;
+    public RaceMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        this.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
