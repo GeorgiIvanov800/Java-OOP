9b03b7e9b7d821d337e4bbe67e29997b0ab82a73
Georgi Ivanov <gogata92@pm.me>
2023-03-03 10:04:11 +0100

    New Task Animals

---
+++ Tasks/src/Animals/Kitten.java
@@ -0,0 +1,8 @@
+package Animals;
+
+public class Kitten extends  Cat {
+    public Kitten(String name, int age) {
+        super(name, age, "Female");
+        this.sound = "Meow";
+    }
+}
---
+++ Tasks/src/Animals/Tomcat.java
@@ -0,0 +1,8 @@
+package Animals;
+
+public class Tomcat extends Cat {
+    public Tomcat(String name, int age) {
+        super(name, age, "Male");
+        this.sound = "MEOW";
+    }
+}
