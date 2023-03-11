4a15662e8cf017829a70a949297c18dbc6adb4b1
Georgi Ivanov <gogata92@pm.me>
2023-03-11 19:06:59 +0100

    Created new Abstract Class CommandHandler

---
+++ Tasks/src/BirthdayCelebrations/CommandHandler.java
@@ -0,0 +1,25 @@
+package BirthdayCelebrations;
+
+import java.util.Scanner;
+
+public abstract class CommandHandler {
+    private final String endCommand;
+    public CommandHandler(String endCommand){
+        this.endCommand = endCommand;
+    }
+
+    public void readAndHandleCommandsToEnd(Scanner scanner) {
+        boolean reachedEnd = this.handleCommand(scanner.nextLine());
+        while (!reachedEnd) {
+            reachedEnd = this.handleCommand(scanner.nextLine());
+        }
+    }
+
+    public boolean handleCommand(String command) {
+        if (command.equals(this.endCommand)) {
+            return true;
+        }
+        return false;
+    }
+    protected abstract void processNonEndCommand(String command);
+}
--- Tasks/src/BirthdayCelebrations/Main.java
+++ Tasks/src/BirthdayCelebrations/Main.java
@@ -1,7 +1,14 @@
 package BirthdayCelebrations;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BirthdayCelebratioinsHandler handler = new BirthdayCelebratioinsHandler("End");
+
+        handler.readAndHandleCommandsToEnd(scanner);
+
 
     }
 }
