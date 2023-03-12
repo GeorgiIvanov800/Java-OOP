531f5328349f7fbd26a9c55e74a92c51d76576bd
Georgi Ivanov <gogata92@pm.me>
2023-03-12 17:51:31 +0100

    Update Main.java

--- Tasks/src/FoodShortage/Main.java
+++ Tasks/src/FoodShortage/Main.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         List<BuyingPerson> buyingPeople = new ArrayList<>();
 
         int numPeople = Integer.parseInt(scanner.nextLine());
