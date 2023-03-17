c97af9e653dc83acc537780821dd9331649624c6
Georgi Ivanov <gogata92@pm.me>
2023-03-17 16:32:21 +0100

    Update Main.java

--- Tasks/src/Reflection/blackBoxInteger/Main.java
+++ Tasks/src/Reflection/blackBoxInteger/Main.java
@@ -1,7 +1,21 @@
 package Reflection.blackBoxInteger;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        Class<BlackBoxInt> blackBoxIntClass = BlackBoxInt.class;
+
+
+        String command = scanner.nextLine();
+        while (!"END".equals(command)) {
+            String[] commandParts = command.split("_");
+            String methodName = commandParts[0];
+            int parameter = Integer.parseInt(commandParts[1]);
+
+            command = scanner.nextLine();
+        }
     }
 }
