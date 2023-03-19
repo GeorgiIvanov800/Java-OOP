916765c1693479e8546f8e619d3ec258c30dfc1d
Georgi Ivanov <gogata92@pm.me>
2023-03-19 14:09:01 +0100

    Update Main.java

--- Tasks/src/Reflection/blackBoxInteger/Main.java
+++ Tasks/src/Reflection/blackBoxInteger/Main.java
@@ -1,7 +1,9 @@
 package Reflection.blackBoxInteger;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Main {
@@ -9,14 +11,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         Class<BlackBoxInt> blackBoxIntClass = BlackBoxInt.class;
+        BlackBoxInt blackBoxInt;
         try {
             Constructor<BlackBoxInt> declaredConstructor = blackBoxIntClass.getDeclaredConstructor();
-            declaredConstructor.newInstance();
-        } catch (NoSuchMethodException |IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            blackBoxInt = declaredConstructor.newInstance();
+        } catch (NoSuchMethodException | IllegalAccessException | InstantiationException |
+                 InvocationTargetException e) {
             throw new IllegalStateException(e);
         }
-
-        BlackBoxInt blackBoxInt = new BlackBoxInt();
 
 
         String command = scanner.nextLine();
@@ -24,6 +26,22 @@ public class Main {
             String[] commandParts = command.split("_");
             String methodName = commandParts[0];
             int parameter = Integer.parseInt(commandParts[1]);
+            try {
+                Method declaredMethod = blackBoxIntClass.getDeclaredMethod(methodName, Integer.class);
+                declaredMethod.invoke(blackBoxInt, parameter);
+            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                throw new IllegalStateException(e);
+            }
+
+            int currentValue;
+            try {
+                Field innerValue = blackBoxIntClass.getDeclaredField("innerValue");
+                currentValue = innerValue.getInt(blackBoxInt);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(currentValue);
 
             command = scanner.nextLine();
         }
